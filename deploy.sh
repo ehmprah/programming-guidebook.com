faa0494e0c8e59b427ff27c6bc8aaca45fd770c7
Mario Kaiser <ehmprah@localhost.localdomain>
2019-10-22 20:19:06 +0200

    Update packages and deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,13 @@ npm run build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'handbuch-programmieren.de' > CNAME
+echo 'programming-guidebook.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# HTTPS
-#git push -f https://github.com/ehmprah/handbuch-programmieren.de.git master:gh-pages
-
 # SSH
-git push -f git@github.com:ehmprah/handbuch-programmieren.de.git master:gh-pages
-
+git push -f git@github.com:ehmprah/programming-guidebook.com.git master:gh-pages
 
 cd -
